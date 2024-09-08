fec6d1212e341c63993795c052013cd925684996
Jacinto Wong <contact@jacinto.design>
2024-09-08 12:13:51 -0400

    added path require

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 
 dotenv.config();
 
